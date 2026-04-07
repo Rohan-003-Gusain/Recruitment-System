264cfc8c52bae6d52768cbcdb100bc2dd6eac1f1
Rohan-003-Gusain <rohangusain95@gmail.com>
2026-04-07 09:46:50 +0530

    update

--- src/main/java/com/recruitment/config/SpringSecurity.java
+++ src/main/java/com/recruitment/config/SpringSecurity.java
@@ -46,6 +46,7 @@ public class SpringSecurity {
             .authorizeHttpRequests(auth -> auth
             		
             		.requestMatchers(
+            				"/",
             		        "/v3/api-docs/**",
             		        "/swagger-ui/**",
             		        "/swagger-ui.html"
